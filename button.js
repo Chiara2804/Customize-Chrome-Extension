dcb36d5eaf3dcfbe2b60e7c1754dfd74c2da6007
Chiara2804 <67204795+Chiara2804@users.noreply.github.com>
2022-04-26 23:04:50 +0200

    Delete button js

--- button.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("recButton").addEventListener("click", function() {
-    if (document.getElementById("recButton").classList.contains("Rec")) {
-        document.getElementById("recButton").classList.remove("Rec");
-        document.getElementById("recButton").classList.add("notRec");
-    } else {
-        document.getElementById("recButton").classList.remove("notRec");
-        document.getElementById("recButton").classList.add("Rec");
-    }
-});
